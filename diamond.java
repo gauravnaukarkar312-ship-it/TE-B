689d66de73b1a0df5aa1c276fd31f508882ea9b2
gauravnaukarkar312-ship-it <gauravnaukarkar312@gmail.com>
2026-03-17 13:52:37 +0530

    Create diamond.java

---
+++ diamond.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Top half
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

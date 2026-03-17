a6cbb856b107f60f3978b813e8966b8a493a0b28
gauravnaukarkar312-ship-it <gauravnaukarkar312@gmail.com>
2026-03-17 13:18:50 +0530

    Create fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int n=10;
+        int one =0;
+        int two =1;
+        for(int i=0;i<n;i++){
+            System.out.println(one + " ");
+            int next = one+two;
+             one=two;
+             two=next;
+        }
+    }
+}
